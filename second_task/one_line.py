2164f860b6799b226ea2c9698457b228f7ad41d4
DmytroIvashchenko <104367979+DmytroIvashchenko@users.noreply.github.com>
2023-11-29 18:27:28 +0200

    Correct one_line file

--- second_task/one_line.py
+++ second_task/one_line.py
@@ -1,9 +1,8 @@
+# this completely contradicts the principles of writing clean code, that it is just a joke
 import statistics
-import csv
-from const import file_path
 
-with open(file_path, "r", encoding="UTF-8") as f:
-    data_values = [float(row[0]) for row in csv.reader(f)]
-
-print(f'Mean - {statistics.mean(data_values)}, Median - {statistics.median(data_values)}'
-      f'\nMode -  {statistics.mode(data_values)}, Standard deviation {statistics.stdev(data_values)}')
+print(
+    f'Mean - {statistics.mean([364, 373, 358, 394, 378, 379, 357, 364, 350, 363, 392, 368, 359, 375, 399, 365, 379, 357, 380])}'
+    f', Median - {statistics.median([364, 373, 358, 394, 378, 379, 357, 364, 350, 363, 392, 368, 359, 375, 399, 365, 379, 357, 380])}'
+    f'\nMode -  {statistics.mode([364, 373, 358, 394, 378, 379, 357, 364, 350, 363, 392, 368, 359, 375, 399, 365, 379, 357, 380])}, '
+    f'Standard deviation {statistics.stdev([364, 373, 358, 394, 378, 379, 357, 364, 350, 363, 392, 368, 359, 375, 399, 365, 379, 357, 380])}')
